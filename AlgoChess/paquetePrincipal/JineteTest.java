1841a1bab18297ae46b2700c3c6ba1d6024e99ad
fedepenic <fedepenic@hotmail.com>
2019-11-04 11:19:03 -0300

    Agregamos Prueba de Soldado.

--- AlgoChess/paquetePrincipal/JineteTest.java
+++ AlgoChess/paquetePrincipal/JineteTest.java
@@ -1,6 +1,18 @@
 package paquetePrincipal;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class JineteTest {
 
-    public void test
+    @Test
+    public void testJineteAtacaSoldadoEnemigoCorrectamente(){
+        Jinete unJinete = new Jinete();
+        Soldado enemigo = new Soldado();
+
+        unJinete.atacar(enemigo);
+
+        Assert.assertEquals(95, enemigo.getVida());
+    }
+
 }
