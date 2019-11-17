10c8ce8b69fa12ce70a173d6c6e5c674198ac7a3
andi-carretero <andi_carretero@hotmail.com>
2019-11-17 17:47:10 -0300

    unidades de batallon ya no se superposicionan entre sí al desplazarse

--- AlgoChess/Tests/BatallonTest.java
+++ AlgoChess/Tests/BatallonTest.java
@@ -48,6 +48,48 @@ public class BatallonTest {
     }
 
     @Test
+    public void testUnSoldadoNoSeMueveSiColisionaConOtroPeroLosOtrosSi(){
+
+        Tablero unTablero = new Tablero(20,20,"Jugador1","Jugador2");
+
+        Posicion posicionUno = new Posicion(0,1);
+        Posicion posicionDos = new Posicion(0,2);
+        Posicion posicionTres = new Posicion(0,3);
+        Posicion posicionCatapulta = new Posicion(1,1);
+
+
+        UnidadMovible soldadoUno = new Soldado(posicionUno,"Jugador1");
+        UnidadMovible soldadoDos = new Soldado(posicionDos,"Jugador1");
+        UnidadMovible soldadoTres = new Soldado(posicionTres,"Jugador1");
+
+        Unidad catapulpa = new Catapulta(posicionCatapulta,"Jugador1");
+
+        Posicion nuevaPosicionUno = new Posicion(1,1);
+        Posicion nuevaPosicionDos = new Posicion(1,2);
+        Posicion nuevaPosicionTres = new Posicion(1,3);
+
+        ArrayList<UnidadMovible> soldados = new ArrayList<>();
+
+        soldados.add(soldadoUno);
+        soldados.add(soldadoDos);
+        soldados.add(soldadoTres);
+
+        unTablero.colocarUnidad(soldadoUno);
+        unTablero.colocarUnidad(soldadoDos);
+        unTablero.colocarUnidad(soldadoTres);
+        unTablero.colocarUnidad(catapulpa);
+
+        Batallon batallon = new Batallon(soldados,unTablero);
+        batallon.moverCentroA(nuevaPosicionDos);    //La posicion central del batallon es la dos,asique muevo esta
+
+        boolean noMovioSoldadoUno = ((soldadoUno.getPosicion().calcularDistancia(posicionUno)) == 0 );
+        boolean movioBienSoldadoDos = ((soldadoDos.getPosicion().calcularDistancia(nuevaPosicionDos)) == 0 );
+        boolean movioBienSoldadoTres = ((soldadoTres.getPosicion().calcularDistancia(nuevaPosicionTres)) == 0 );
+
+        Assert.assertTrue( noMovioSoldadoUno && movioBienSoldadoDos && movioBienSoldadoTres);
+    }
+
+    @Test
     public void testMuevoCorrectamenteLosSoldadosConPosicionesDistintasYSuperposicionEnElMedio(){
 
         Tablero unTablero = new Tablero(20,20,"Jugador1","Jugador2");
@@ -90,27 +132,22 @@ public class BatallonTest {
         Assert.assertTrue( movioBienSoldadoUno && noMovioSoldadoDos && movioBienSoldadoTres);
     }
 
-
     @Test
-    public void testUnSoldadoNoSeMueveSiColisionaConOtroPeroLosOtrosSi(){
+    public void testSoldadosNoSeSuperponenEntreSiAlMoverse(){
 
         Tablero unTablero = new Tablero(20,20,"Jugador1","Jugador2");
 
-        Posicion posicionUno = new Posicion(0,1);
-        Posicion posicionDos = new Posicion(0,2);
-        Posicion posicionTres = new Posicion(0,3);
-        Posicion posicionCatapulta = new Posicion(1,1);
-
+        Posicion posicionUno = new Posicion(1,1);
+        Posicion posicionDos = new Posicion(2,1);
+        Posicion posicionTres = new Posicion(3,1);
 
         UnidadMovible soldadoUno = new Soldado(posicionUno,"Jugador1");
         UnidadMovible soldadoDos = new Soldado(posicionDos,"Jugador1");
         UnidadMovible soldadoTres = new Soldado(posicionTres,"Jugador1");
 
-        Unidad catapulpa = new Catapulta(posicionCatapulta,"Jugador1");
-
-        Posicion nuevaPosicionUno = new Posicion(1,1);
-        Posicion nuevaPosicionDos = new Posicion(1,2);
-        Posicion nuevaPosicionTres = new Posicion(1,3);
+        Posicion nuevaPosicionUno = new Posicion(2,1);
+        Posicion nuevaPosicionDos = new Posicion(3,1);
+        Posicion nuevaPosicionTres = new Posicion(4,1);
 
         ArrayList<UnidadMovible> soldados = new ArrayList<>();
 
@@ -121,16 +158,15 @@ public class BatallonTest {
         unTablero.colocarUnidad(soldadoUno);
         unTablero.colocarUnidad(soldadoDos);
         unTablero.colocarUnidad(soldadoTres);
-        unTablero.colocarUnidad(catapulpa);
 
         Batallon batallon = new Batallon(soldados,unTablero);
         batallon.moverCentroA(nuevaPosicionDos);    //La posicion central del batallon es la dos,asique muevo esta
 
-        boolean noMovioSoldadoUno = ((soldadoUno.getPosicion().calcularDistancia(posicionUno)) == 0 );
+        boolean movioBienSoldadoUno = ((soldadoUno.getPosicion().calcularDistancia(nuevaPosicionUno)) == 0 );
         boolean movioBienSoldadoDos = ((soldadoDos.getPosicion().calcularDistancia(nuevaPosicionDos)) == 0 );
         boolean movioBienSoldadoTres = ((soldadoTres.getPosicion().calcularDistancia(nuevaPosicionTres)) == 0 );
 
-        Assert.assertTrue( noMovioSoldadoUno && movioBienSoldadoDos && movioBienSoldadoTres);
+        Assert.assertTrue( movioBienSoldadoUno && movioBienSoldadoDos && movioBienSoldadoTres);
     }
 
 
